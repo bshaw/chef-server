d39dd54bef2f42dedc35b9714ccf9177f61774b7
Brian Shaw <bshaw@virtusa.com>
2017-10-01 13:04:35 +0100

    Update installation script

--- chef-install.sh
+++ chef-install.sh
@@ -1,48 +1,47 @@
-#set hostname
+# set hostname
 sudo hostname chef.vbox.local
 sudo sed -i '/HOSTNAME=/c\HOSTNAME=chef.vbox.local' /etc/sysconfig/network
 sudo echo -e "127.0.0.1 `hostname` `hostname -s`" | sudo tee -a /etc/hosts
 sudo /etc/init.d/network restart
 
-#install deps
-sudo yum -y install git curl wget tar vim
-
-#update
+# update
 sudo yum -y update
 
-#install chef-solo
+# install dep packages
+sudo yum -y install git curl wget tar
+
+# install chef-solo
 curl -L https://www.chef.io/chef/install.sh | sudo bash
 
-#create required bootstrap dirs/files
+# create required bootstrap dirs/files
 sudo mkdir -p /var/chef/cache /var/chef/cookbooks
 
-#pull down chef-server cookbook
+# pull down chef-server cookbook
 wget -qO- https://supermarket.chef.io/cookbooks/chef-server/download | sudo tar xvzC /var/chef/cookbooks
 
-#pull down dependency cookbooks
-wget -qO- https://supermarket.chef.io/cookbooks/chef-server-ingredient/download | sudo tar xvzC /var/chef/cookbooks
-wget -qO- https://supermarket.chef.io/cookbooks/chef-ingredient/download | sudo tar xvzC /var/chef/cookbooks
-wget -qO- https://supermarket.chef.io/cookbooks/packagecloud/download | sudo tar xvzC /var/chef/cookbooks
-wget -qO- https://supermarket.chef.io/cookbooks/apt/download | sudo tar xvzC /var/chef/cookbooks
-wget -qO- https://supermarket.chef.io/cookbooks/apt-chef/download | sudo tar xvzC /var/chef/cookbooks
-wget -qO- https://supermarket.chef.io/cookbooks/yum/download | sudo tar xvzC /var/chef/cookbooks
-wget -qO- https://supermarket.chef.io/cookbooks/yum-chef/download | sudo tar xvzC /var/chef/cookbooks
-wget -qO- https://supermarket.chef.io/cookbooks/compat_resource/download | sudo tar xvzC /var/chef/cookbooks
+# pull down dependency cookbooks
+for dep in yum-chef yum apt-chef apt packagecloud compat_resource
+do
+  wget -qO- https://supermarket.chef.io/cookbooks/${dep}/download | sudo tar xvzC /var/chef/cookbooks
+done
 
-#install chef-server
+knife cookbook site download chef-ingredient 1.1.0
+sudo tar xvzf chef-ingredient-1.1.0.tar.gz -C /var/chef/cookbooks && rm -f chef-ingredient-1.1.0.tar.gz
+
+# install chef-server
 sudo chef-solo -o 'recipe[chef-server::default]'
 
-#create an admin user
+# create an admin user
 sudo chef-server-ctl user-create --filename admin.pem admin Chef Admin dev@null.com Password1!
 
-#create an organization
+# create an organization
 sudo chef-server-ctl org-create vagrant "Vagrant" --association_user admin --filename vagrant.pem
 
-#setup chef server as a client of its self
+# setup chef server as a client of its self
 sudo cp vagrant.pem /etc/chef/validation.pem
 knife ssl fetch
 sudo cp -R /root/.chef/trusted_certs/ /etc/chef/
 
-#copy keys to host
+# copy keys to host
 cp admin.pem /vagrant/.chef/
 cp vagrant.pem /vagrant/.chef/
